2f86de35540bc99b8b4ed819942a064cc14af84d
SerjBerlin <bulanovsd@mail.ru>
2026-09-14 10:23:19 +0300

    Перенес логику приложения в src/index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import _ from "lodash";
+import {app as startApp }from './src/index.js'
 
-console.log("Hello, Hexlet!");
-console.log(_.last(['one', 'two']));
+startApp()
---
+++ src/index.js
@@ -0,0 +1,4 @@
+export const app = () => {
+    console.log("Hello, hexlet!")
+}
+
